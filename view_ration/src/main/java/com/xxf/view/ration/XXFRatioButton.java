a514f6dab1b37ca8d9dec33f8420d1357b81e4f9
XXF <xuanyouwu@163.com>
2021-02-04 15:25:28 +0800

    feat:支持宽高比例的组件

---
+++ view_ration/src/main/java/com/xxf/view/ration/XXFRatioButton.java
@@ -0,0 +1,61 @@
+package com.xxf.view.ration;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.xxf.roundcomponent.XXFCompatButton;
+import com.xxf.view.ration.inner.RatioDatumMode;
+import com.xxf.view.ration.inner.RatioLayoutDelegate;
+import com.xxf.view.ration.inner.XXFRatioWidget;
+
+
+/**
+ * @Author: XGod  xuanyouwu@163.com  17611639080
+ * Date: 2/4/21 2:45 PM
+ * Description: TODO
+ */
+public class XXFRatioButton extends XXFCompatButton implements XXFRatioWidget {
+
+    private RatioLayoutDelegate mRatioLayoutDelegate;
+
+    public XXFRatioButton(Context context) {
+        super(context);
+    }
+
+    public XXFRatioButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mRatioLayoutDelegate = RatioLayoutDelegate.obtain(this, attrs);
+    }
+
+    public XXFRatioButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mRatioLayoutDelegate = RatioLayoutDelegate.obtain(this, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (mRatioLayoutDelegate != null) {
+            mRatioLayoutDelegate.update(widthMeasureSpec, heightMeasureSpec);
+            widthMeasureSpec = mRatioLayoutDelegate.getWidthMeasureSpec();
+            heightMeasureSpec = mRatioLayoutDelegate.getHeightMeasureSpec();
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    public void setRatio(RatioDatumMode mode, float widthRatio, float heightRatio) {
+        if (mRatioLayoutDelegate != null) {
+            mRatioLayoutDelegate.setRatio(mode, widthRatio, heightRatio);
+        }
+    }
+
+
+
+    @Override
+    public void setAspectRatio(float aspectRatio) {
+        if (mRatioLayoutDelegate != null) {
+            mRatioLayoutDelegate.setAspectRatio(aspectRatio);
+        }
+    }
+}
---
+++ view_ration/src/main/java/com/xxf/view/ration/inner/XXFRatioWidget.java
@@ -0,0 +1,13 @@
+package com.xxf.view.ration.inner;
+
+/**
+ * @Author: XGod  xuanyouwu@163.com  17611639080
+ * Date: 2/4/21 2:45 PM
+ * Description: TODO
+ */
+public interface XXFRatioWidget {
+
+    void setRatio(RatioDatumMode mode, float widthRatio, float heightRatio);
+
+    void setAspectRatio(float aspectRatio);
+}
